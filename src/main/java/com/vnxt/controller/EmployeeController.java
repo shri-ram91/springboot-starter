eb767335ad450391acf209b4c302910f5a6e0e8b
Shriraman V <shriraman.v@variablenxt.com>
2023-07-25 06:09:12 +0530

    Initial commit

--- src/main/java/com/vnxt/controller/EmployeeController.java
+++ src/main/java/com/vnxt/controller/EmployeeController.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/article")
+@RequestMapping("api/employee")
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class EmployeeController {
